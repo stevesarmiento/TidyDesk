44201b919ab9ae1daa070908086295a4cbc1ade4
Steven Sarmiento <sarmiento.steven@gmail.com>
2024-02-11 23:59:14 -0500

    Initial Commit

---
+++ TidyDesk/TidyDeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TidyDeskApp.swift
+//  TidyDesk
+//
+//  Created by Steven Sarmiento on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TidyDeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
