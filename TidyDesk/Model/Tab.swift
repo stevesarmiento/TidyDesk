ebe2aa9e8414d5681c54e8adffff481be7cc4641
steven <sarmiento.steven@gmail.com>
2024-02-12 04:02:21 -0500

    playgorund with sidebar functionality

---
+++ TidyDesk/Model/Tab.swift
@@ -0,0 +1,18 @@
+//
+//  Tab.swift
+//  TidyDesk
+//
+//  Created by Steven Sarmiento on 2/12/24.
+//
+
+import SwiftUI
+
+struct Tab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tab()
+}
---
+++ TidyDesk/View/CustomTabView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabView.swift
+//  TidyDesk
+//
+//  Created by Steven Sarmiento on 2/12/24.
+//
+
+import SwiftUI
+
+struct CustomTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabView()
+}
